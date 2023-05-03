468a3353f7bc211c289f3a72aa996dad349db5d8
CUTE BACCHA <132035388+CuteBacchaXD@users.noreply.github.com>
2023-05-03 16:12:06 +0530

    Update bot.py

--- Romeo/core/bot.py
+++ Romeo/core/bot.py
@@ -8,7 +8,7 @@ from ..logging import LOGGER
 
 
 
-class RomeoBot(Client):
+class cherry music(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
